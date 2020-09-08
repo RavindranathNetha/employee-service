7c776128a85c677fbe8cd3a5960f4089ea27d93a
ajayrahulgodithi <ajayrahul.godithi@gmail.com>
2020-09-07 21:11:36 -0500

    Updated Swagger

--- src/main/java/com/learning/ark/training/TrainingApplication.java
+++ src/main/java/com/learning/ark/training/TrainingApplication.java
@@ -1,13 +1,26 @@
 package com.learning.ark.training;
 
+import com.learning.ark.training.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class TrainingApplication {
+@EnableSwagger2
+public class TrainingApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
 	}
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Inside run");
+		//System.out.println(employeeRepository.findAll());
+	}
 }
